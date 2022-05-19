bea24b6a06eb8a300e82d23af100c5bc9cf0b4c6
Jawid <41361157+infjay@users.noreply.github.com>
2022-05-19 14:39:21 +0200

    Patient model added

---
+++ models/Patient.model.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const { Schema, model } = mongoose;
+
+const patientSchema = new Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    birthDate: {
+        type: Date,
+        required: true,
+    },
+    bloodType: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true
+    },
+
+    
+});
+
+
+module.exports = model("Patinet", patientSchema);
